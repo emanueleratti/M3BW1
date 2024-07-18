09071941ca406ee740c9bc2cd7b3100757c1b91b
emanueleratti <hello@emanueleratti.it>
2024-07-18 21:55:56 +0200

    merge day 4

--- js/pagina-2.js
+++ js/pagina-2.js
@@ -114,3 +114,12 @@ const cardCreation = (cardData) => {
 apartments.genova.forEach(apartment => {
     cardCreation(apartment);
 })
+
+const menu = document.getElementById('menu')
+const sidebar = document.getElementById('sidebar')
+
+const openMenu = () => {
+  sidebar.classList.toggle('visible')
+}
+
+menu.addEventListener('click', openMenu)
